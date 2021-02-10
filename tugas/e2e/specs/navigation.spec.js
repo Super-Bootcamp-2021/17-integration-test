0fb85f2409195010ab224194b8af68369c48c2f5
puputrizka <puputrizka06@gmail.com>
2021-02-10 20:04:52 +0700

    add url testing

--- tugas/e2e/specs/navigation.spec.js
+++ tugas/e2e/specs/navigation.spec.js
@@ -8,13 +8,16 @@ describe('Pergi ke halaman lain', () => {
     it('seharusnya bisa pergi ke halaman pekerja', () => {
         cy.get('ul').children().as('menu');
         cy.get('@menu').eq(0).children().click();
+        cy.url().should('include', '/worker');
     });
     it('seharusnya bisa pergi ke halaman pekerjaan', () => {
         cy.get('ul').children().as('menu');
         cy.get('@menu').eq(1).children().click();
+        cy.url().should('include', '/tasks');
     });
     it('seharusnya bisa pergi ke halaman kinerja', () => {
         cy.get('ul').children().as('menu');
         cy.get('@menu').eq(2).children().click();
+        cy.url().should('include', '/performance');
     });
 })
