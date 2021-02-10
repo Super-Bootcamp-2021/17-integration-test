aedf4f9a952691469314050c916c8360dee3b30a
unknown <triadikurniawan560@gmail.com>
2021-02-10 17:02:18 +0800

    add edge test of intergration test performance

--- tugas/e2e/specs/performance.spec.js
+++ tugas/e2e/specs/performance.spec.js
@@ -1,6 +1,7 @@
 describe('performance page', () =>{
     it('can open performance page', () => {
         cy.visit('/');
+        cy.get('#menuPerformance').click();
     })
 
     describe('performance list', () => {
@@ -16,6 +17,7 @@ describe('performance page', () =>{
                 }, 
                 },  
             );
+            cy.visit('/');
             cy.get('#menuPerformance').click();
             cy.get('#tasks').should('contain.text', '5');
             cy.get('#task-done').should('contain.text', '2');
@@ -35,9 +37,7 @@ describe('performance page', () =>{
         })
 
         it('seharusnya bisa menampilkan summary of performance lagi ketika button refresh diklik', () => {
-            //cy.visit('/');
-
-
+  
             cy.intercept('get','/summary', {fixture: 'performance'}) 
         
             cy.get('#tasks').should('contain.text', '5');
@@ -45,5 +45,36 @@ describe('performance page', () =>{
             cy.get('#task-canceled').should('contain.text', '8');
             cy.get('#workers').should('contain.text', '6');
         })
+
     })
+
+    describe('loading and error message', () => {
+        it('seharusnya bisa menampilkan loading ketika sedang load data', () => {
+           cy.visit('/');
+           cy.get('#menuPerformance').click();  
+            cy.get('#tasks').should('contain.text', '0');
+            cy.get('#task-done').should('contain.text', '0');
+            cy.get('#task-canceled').should('contain.text', '0');
+            cy.get('#workers').should('contain.text', '0');
+            cy.get('#loading-text').should('contain.text', 'memuat...');
+        })
+        it('seharusnya bisa menampilkan error ketika gagal load data', () => {
+           cy.visit('/');
+           cy.get('#menuPerformance').click();  
+            cy.intercept(
+                {method: 'GET', pathname: '/summary', 
+                }, 
+                {body: { total_task : 5, task_done : 2, task_cancelled : 8, total_worker : 6 },statusCode: 404,
+                headers: {
+                    'content-type': 'application/json',
+                }, 
+                },  
+            );
+            cy.get('#tasks').should('contain.text', '0');
+            cy.get('#task-done').should('contain.text', '0');
+            cy.get('#task-canceled').should('contain.text', '0');
+            cy.get('#workers').should('contain.text', '0');
+            cy.get('#error-text').should('contain.text', 'gagal memuat informasi kinerja');
+        })
+    })  
 })
