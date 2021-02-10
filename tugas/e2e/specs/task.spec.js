cc37611176019aa9c5868bb2dd96adc96c7e6905
Muhammad Risky Nugraha <muhammadrisky83@gmail.com>
2021-02-10 21:59:42 +0700

    added task test (add, done, cancel)

--- tugas/e2e/specs/task.spec.js
+++ tugas/e2e/specs/task.spec.js
@@ -1,19 +1,23 @@
 require('../../cypress/support');
 
+function init() {
+  cy.intercept('http://localhost:7002/list', {
+    fixture: 'tasks',
+  }).as('getTasks');
+  cy.intercept('http://localhost:7001/list', {
+    fixture: 'workers',
+  }).as('getWorkers');
+  cy.intercept('/attachment', { fixture: 'images/example.jpg' }).as(
+    'getAttachment'
+  );
+  cy.visit('/tasks.html');
+}
+
 describe('Task E2E Testing', () => {
   describe('List Tasks', () => {
     it('Should show list task & worker', () => {
-      cy.intercept('http://localhost:7002/list', { fixture: 'tasks' }).as(
-        'getTaskList'
-      );
-      cy.intercept('http://localhost:7001/list', {
-        fixture: 'workers',
-      }).as('getWorkerList');
-      cy.intercept('/attachment', { fixture: 'images/example.jpg' }).as(
-        'getAttachment'
-      );
+      init();
 
-      cy.visit('/tasks.html');
       cy.get('#list').children().as('taskList');
       cy.get('@taskList').should('have.length', 3);
 
@@ -34,28 +38,13 @@ describe('Task E2E Testing', () => {
   });
 
   describe('Add task', () => {
-    beforeEach(() => {
-      cy.intercept('http://localhost:7002/list', { fixture: 'tasks' }).as(
-        'getTasks'
-      );
-      cy.intercept('http://localhost:7001/list', {
-        fixture: 'workers',
-      }).as('getWorkers');
-      cy.intercept('/attachment', { fixture: 'images/example.jpg' }).as(
-        'getAttachment'
-      );
-      cy.visit('/tasks.html');
-    });
+    // beforeEach(() => {
+    //   init();
+    // });
 
     it('should add when submit', () => {
-        // cy.wait('@getTasks')
-        // cy.wait('@getWorkers')
-        // cy.wait('@getAttachment')
-      cy.intercept(
-        { pathname: '/add', method: 'POST' },
-        { fixture: 'task' }
-      ).as('addTask');
-      
+      cy.intercept('/add', { fixture: 'task' }).as('addTask');
+
       cy.fixture('images/example.jpg').then((fileContent) => {
         cy.get('input[type="file"]').attachFile({
           fileContent: fileContent.toString(),
@@ -66,11 +55,77 @@ describe('Task E2E Testing', () => {
 
       cy.get('#job').type('play');
       cy.get('#assignee').select('1');
-      cy.get('#submit_task').click();
-    // cy.get('#form').submit()
+      cy.get('#form').submit();
 
-    //   cy.wait('@addTask');
-           
+      cy.wait('@addTask');
+    });
+  });
+
+  describe('Done Task Action', () => {
+    beforeEach(() => {
+      init();
+    });
+
+    afterEach(function (done) {
+      done();
+    });
+
+    it('Should done when clicked', () => {
+      cy.intercept('/done', { fixture: 'task' }).as('doneTask');
+      cy.get('#list').children().eq(0).children().eq(4).as('doneBtn');
+      cy.get('@doneBtn').click();
+      cy.wait('@doneTask');
+      cy.get('#list').should('include.text', 'sudah selesai');
+    });
+
+    it("Shouldn't done when clicked", () => {
+      cy.get('#list').children().eq(0).children().eq(4).as('doneBtn2');
+      cy.get('@doneBtn2').click();
+      cy.get('#error-text').should('include.text', 'gagal');
+      cy.get('#list').should('not.include.text', 'sudah selesai');
+    });
+  });
+
+  describe('Cancel Task Action', () => {
+    // beforeEach(() => {
+    //   init();
+    // });
+
+    afterEach(function (done) {
+      done();
+    });
+
+    it('Should cancel when clicked', () => {
+      cy.intercept('/cancel', { fixture: 'task' }).as('cancelTask');
+      cy.get('#list').children().eq(1).children().eq(3).as('cancelBtn');
+      cy.get('@cancelBtn').click();
+      cy.wait('@cancelTask');
+    });
+
+    it("Shouldn't cancel when clicked", () => {
+      cy.get('#list').children().eq(0).children().eq(4).as('cancelBtn2');
+      cy.get('@cancelBtn2').click();
+      cy.get('#error-text').should('include.text', 'gagal');
+    });
+  });
+  describe.skip('Navigation test', () => {
+    beforeEach(() => {
+      cy.visit('/');
+    });
+
+    it('Should visit worker', () => {
+      cy.get(':nth-child(1) > a').click();
+      cy.url().should('eq', 'http://localhost:7000/worker.html');
+    });
+
+    it('Should visit task', () => {
+      cy.get(':nth-child(2) > a').click();
+      cy.url().should('eq', 'http://localhost:7000/tasks.html');
+    });
+
+    it('Should visit performance', () => {
+      cy.get(':nth-child(3) > a').click();
+      cy.url().should('eq', 'http://localhost:7000/performance.html');
     });
   });
 });
