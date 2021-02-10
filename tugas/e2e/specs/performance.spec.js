c1dfd830570ff5df0d71afc7cfcd2c69d901be58
puputrizka <puputrizka06@gmail.com>
2021-02-10 17:41:12 +0700

    add testing refresh performance

--- tugas/e2e/specs/performance.spec.js
+++ tugas/e2e/specs/performance.spec.js
@@ -3,14 +3,20 @@ describe('Performance Page', () => {
         cy.visit('/performance.html')
     });
 
-    describe('Refresh Halaman', () => {
+    describe('Tampilkan summary', () => {
         it('seharusnya bisa tampilkan request summary', () => {
             cy.intercept('GET','http://localhost:7003/summary', {
                 total_task: 0,
                 task_done: 0,
                 task_cancelled: 0,
-                total_worker: 0, 
+                total_worker: 0,        
             }); 
+            cy.get('#refresh').click().should({
+                total_task: 20,
+                task_done: 12,
+                task_cancelled: 2,
+                total_worker: 23,  
+            }) 
         });
     });
 });
