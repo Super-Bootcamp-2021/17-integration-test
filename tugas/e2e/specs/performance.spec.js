a36e1067a75625d9a62dfd154929f83afd7c48cf
puputrizka <puputrizka06@gmail.com>
2021-02-10 18:14:58 +0700

    fix performance testing

--- tugas/e2e/specs/performance.spec.js
+++ tugas/e2e/specs/performance.spec.js
@@ -1,25 +1,27 @@
 describe('Performance Page', () => {
     it('seharusnya bisa membuka halaman performance', () => {
-        cy.visit('/performance.html')
+        cy.visit('/performance.html');
     });
 
-    describe('Tampilkan summary', () => {
-        it('seharusnya bisa tampilkan request summary', () => {
+    describe('Refresh Page', () => {
+        it('seharusnya bisa me-refresh halaman', () => {
             cy.intercept('GET','http://localhost:7003/summary', {
-                total_task: 0,
-                task_done: 0,
-                task_cancelled: 0,
-                total_worker: 0,        
+                total_worker: 20,
+                total_task: 10,
+                task_done: 7,
+                task_cancelled: 3,        
             }); 
-            cy.get('#refresh').click().should({
-                total_task: 20,
-                task_done: 12,
-                task_cancelled: 2,
-                total_worker: 23,  
-            }) 
-        });
+            cy.visit('/performance.html');
+            cy.get('#refresh').click();
+            cy.get('ul').last().as('jumlahPekerja');
+            cy.get('@jumlahPekerja').children().eq(0).should('contain.text', '20')
+            cy.get('@jumlahPekerja').children().eq(1).should('contain.text', '10')
+            cy.get('@jumlahPekerja').children().eq(2).should('contain.text', '7')
+            cy.get('@jumlahPekerja').children().eq(3).should('contain.text', '3')
+        }) 
     });
 });
+
 
 
 
